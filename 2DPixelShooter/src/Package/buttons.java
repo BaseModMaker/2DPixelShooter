5499284dcb378d66fdc42c4bb5c14d8bae5bafce
Mr_BASE <Mr_BASE@macvanbasile-3.lan>
2020-04-15 13:05:08 +0200

    moved buttons from main menu class to the button class. Pressing esc doesn't work yet

--- 2DPixelShooter/src/Package/buttons.java
+++ 2DPixelShooter/src/Package/buttons.java
@@ -7,20 +7,17 @@ import java.awt.image.BufferedImage;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class buttons implements ActionListener {
-	ArrayList<String> ButtonNameList = new ArrayList<>(Arrays.asList("Arsenal_NewGun", "Arsenal_Gun1", "Arsenal_Gun2", "Arsenal_Gun3", "Arsenal_Gun4", "Arsenal_Gun5", "Arsenal_Gun6", "Arsenal_Gun7"));
+	ArrayList<String> ButtonNameList = new ArrayList<>(Arrays.asList("Arsenal_NewGun", "Arsenal_Gun1", "Arsenal_Gun2", "Arsenal_Gun3", "Arsenal_Gun4", "Arsenal_Gun5", "Arsenal_Gun6", "Arsenal_Gun7", "MainMenu_Play", "MainMenu_Arsenal"));
 	ArrayList<ArrayList<Integer>> ButtonSpecificationsList = new ArrayList<>();
 	ArrayList<JButton> ButtonList = new ArrayList<>();
 	ArrayList<BufferedImage> imageList = new ArrayList<>();
 	Main main;
-
-
 
 	public buttons(Main main, Arsenal arsenal) {
 		this.main = main;
@@ -35,30 +32,23 @@ public class buttons implements ActionListener {
 					image = null;
 				}
 				imageList.add(image);
-				// imageList.add(new ImageIcon(image.getScaledInstance(arsWidth, arsHeight,
-				// BufferedImage.SCALE_DEFAULT)));
 			}
 		} catch (Exception e) {
 
 		}
-		
-		List<String> arsNames = Arrays.asList("Arsenal_NewGun", "Arsenal_Gun1", "Arsenal_Gun2", "Arsenal_Gun3", "Arsenal_Gun4", "Arsenal_Gun5", "Arsenal_Gun6", "Arsenal_Gun7");
+
+		// List<String> arsNames = Arrays.asList("Arsenal_NewGun", "Arsenal_Gun1",
+		// "Arsenal_Gun2", "Arsenal_Gun3", "Arsenal_Gun4", "Arsenal_Gun5",
+		// "Arsenal_Gun6", "Arsenal_Gun7");
 		for (int i = 0; i < ButtonNameList.size(); i++) {
 			if (ButtonNameList.get(i).split("_")[0].equals("Arsenal")) {
-				ButtonMaker(arsNames.get(i), main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, (main.getWidth() - main.arsButtonWidth) / 2, (i + 1) * main.getHeight() / 8 - main.arsButtonHeight / 2);
+				ButtonMaker(ButtonNameList.get(i), main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, (main.width - main.arsButtonWidth) / 2, (i + 1) * main.height / 8 - main.arsButtonHeight / 2);
 			} else {
 				break;
 			}
 		}
-		/*ButtonMaker("Arsenal_NewGun", main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, main.arsButtonXPos, main.arsButtonYPos);
-		ButtonMaker("Arsenal_Gun1", main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, main.arsButtonXPos, main.arsButtonYPos);
-		ButtonMaker("Arsenal_Gun2", main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, main.arsButtonXPos, main.arsButtonYPos);
-		ButtonMaker("Arsenal_Gun3", main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, main.arsButtonXPos, main.arsButtonYPos);
-		ButtonMaker("Arsenal_Gun4", main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, main.arsButtonXPos, main.arsButtonYPos);
-		ButtonMaker("Arsenal_Gun5", main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, main.arsButtonXPos, main.arsButtonYPos);
-		ButtonMaker("Arsenal_Gun6", main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, main.arsButtonXPos, main.arsButtonYPos);
-		ButtonMaker("Arsenal_Gun7", main.arsButtonWidth, main.arsButtonHeight, main.arsButtonFontSize, main.arsButtonXPos, main.arsButtonYPos);*/
-
+		ButtonMaker("MainMenu_Play", main.mMButtonWidth, main.mMButtonHeight, main.mMButtonFontSize, (main.width - main.mMButtonWidth) / 2, (main.height - main.mMButtonHeight) / 2);
+		ButtonMaker("MainMenu_Arsenal", main.mMButtonWidth, main.mMButtonHeight, main.mMButtonFontSize, (main.width - main.mMButtonWidth) / 2, 6 * main.height / 10 - main.mMButtonHeight / 2);
 	}
 
 	private void ButtonMaker(String name, int width, int height, int fontSize, int xPosition, int yPosition) {
@@ -76,31 +66,90 @@ public class buttons implements ActionListener {
 		if (image != null) {
 			button.setIcon(new ImageIcon(image.getScaledInstance(width, height, BufferedImage.SCALE_DEFAULT)));
 		}
-		button.setBounds(xPosition, yPosition, width, height);
+		// button.setBounds(xPosition, yPosition, width, height);
 		ButtonList.add(button);
 	}
 
-	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().equals("Arsenal_NewGun") == true) {
+		/*
+		 * if (e.getActionCommand().equals("Arsenal_NewGun") == true) {
+		 * 
+		 * // Go from arsenal to gun sprite selection main.arsenal = null;
+		 * main.gunSpriteSelection = new GunSpriteSelection(main, this);
+		 * main.addKeyListener(main.gunSpriteSelection); main.setFocusable(true);
+		 * main.setFocusTraversalKeysEnabled(false); main.SetButtonVisibility("Arsenal",
+		 * false);
+		 * 
+		 * } if(e.getActionCommand().equalsIgnoreCase("MainMenu_Play")) { //Go from
+		 * MainMenu to Game main.mainMenu = null; main.game = new Game (main, this);
+		 * main.addMouseListener(main.game); main.addKeyListener(main.game);
+		 * main.setFocusable(true); main.setFocusTraversalKeysEnabled(false);
+		 * main.SetButtonVisibility("MainMenu", false); }
+		 * if(e.getActionCommand().equalsIgnoreCase("MainMenu_Arsenal")) {
+		 * 
+		 * //Go from main menu to arsenal main.SetButtonVisibility("Arsenal", true);
+		 * main.mainMenu = null; main.arsenal = new Arsenal(main, this);
+		 * main.addKeyListener(main.arsenal); main.setFocusable(true);
+		 * main.setFocusTraversalKeysEnabled(false);
+		 * main.SetButtonVisibility("MainMenu", false); }
+		 */
+		ChangeClass("Arsenal_NewGun", "GunSpriteSelection", e);
+		ChangeClass("MainMenu_Play", "Game", e);
+		ChangeClass("MainMenu_Arsenal", "Arsenal", e);
+	}
 
-			// Go from arsenal to gun sprite selection
-			main.arsenal = null;
-			main.gunSpriteSelection = new GunSpriteSelection(main, this);
-			main.addKeyListener(main.gunSpriteSelection);
+	public void ChangeClass(String buttonName, String newClassName, ActionEvent e) {
+		if (e.getActionCommand().equalsIgnoreCase(buttonName)) {
+			
+			main.SetButtonVisibility(buttonName.split("_")[0], false);
+			main.SetButtonVisibility(newClassName, true);
+			
+			NullifyClass(buttonName);
+			
+			NewClass(newClassName);
+		
 			main.setFocusable(true);
 			main.setFocusTraversalKeysEnabled(false);
-			main.SetButtonVisibility("Arsenal", false);
+		}
 
+	}
+
+	private void NullifyClass(String buttonName) {
+		if (buttonName.split("_")[0].contentEquals("Arsenal")) {
+			main.arsenal = null;
+		}
+		if (buttonName.split("_")[0].contentEquals("MainMenu")) {
+			main.mainMenu = null;
+		}
+		if (buttonName.split("_")[0].contentEquals("GunCategorySelection")) {
+			main.gunCategorySelection = null;
+		}
+		if (buttonName.split("_")[0].contentEquals("GunCreationScreen")) {
+			main.gunCreationScreen = null;
+		}
+	}
+	
+	private void NewClass(String newClassName) {
+		if (newClassName.contentEquals("Arsenal")) {
+			main.arsenal = new Arsenal(main, this);
+			main.addKeyListener(main.arsenal);
+		}
+		if (newClassName.contentEquals("Game")) {
+			main.game = new Game(main, this);
+			main.addMouseListener(main.game);
+			main.addKeyListener(main.game);
+		}
+		if (newClassName.contentEquals("GunSpriteSelection")) {
+			main.gunSpriteSelection = new GunSpriteSelection(main, this);
+			main.addKeyListener(main.gunSpriteSelection);
 		}
 	}
 
 	public void update() {
 		for (int i = 0; i < ButtonNameList.size(); i++) {
-			if (ButtonNameList.get(i).split("_")[0].equals("Arsenal")) {
-				ButtonList.get(i).setBounds((main.getWidth() - main.arsButtonWidth) / 2, (i + 1) * main.getHeight() / 8 - main.arsButtonHeight / 2, main.arsButtonWidth, main.arsButtonHeight);
-			}
+			ArrayList<Integer> list = ButtonSpecificationsList.get(i);
+			ButtonList.get(i).setBounds(list.get(3), list.get(4), list.get(0), list.get(1));
 		}
 	}
 
